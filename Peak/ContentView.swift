00e998bf55fc070bf0b968113703e08473d3ca5e
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2026-01-09 18:14:02 -0800

    Initial Commit

---
+++ Peak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Peak
+//
+//  Created by Enzo on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Peak/PeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakApp.swift
+//  Peak
+//
+//  Created by Enzo on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct PeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
